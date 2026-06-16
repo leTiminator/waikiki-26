7cbefb650df2cb9e007445c0b86dff3cf8a3ba3d
tcoat <timinator49ftw@gmail.com>
2026-06-16 15:58:01 -0700

    fix: cap app to phone-width column so banner is compact + title overlaps; sw v9

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Offline cache for Waikiki '26. Bump VERSION to force an update after edits.
-const VERSION = "waikiki-v8";
+const VERSION = "waikiki-v9";
 const ASSETS = [
   "./",
   "./index.html",
