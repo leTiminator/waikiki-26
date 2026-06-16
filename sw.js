2f164e15bc7b0f27e6bf50d8f34417606b926ceb
tcoat <timinator49ftw@gmail.com>
2026-06-16 15:09:33 -0700

    feat: passcode confirm field + Change-passcode button; bump sw to v6

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Offline cache for Waikiki '26. Bump VERSION to force an update after edits.
-const VERSION = "waikiki-v5";
+const VERSION = "waikiki-v6";
 const ASSETS = [
   "./",
   "./index.html",
