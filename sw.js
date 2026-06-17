65b155909f0f9755e91eb5882e2744f9dd66d3c0
tcoat <timinator49ftw@gmail.com>
2026-06-16 18:16:43 -0700

    feat: header pill = countdown before trip, 'Day N of 14' during; sw v20

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Offline cache for Waikiki '26. Bump VERSION to force an update after edits.
-const VERSION = "waikiki-v19";
+const VERSION = "waikiki-v20";
 const ASSETS = [
   "./",
   "./index.html",
